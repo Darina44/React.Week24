b5f79487ff5429c30702bbd27aa308a80ae893e9
darina bebenina <darinabebenina@yahoo.com>
2023-06-28 16:58:22 +0500

    week24

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button name={"Нажми меня"} className="test" id="buttonTest" title="test" />
+    </div>
+  );
+}
+
+export default App;
